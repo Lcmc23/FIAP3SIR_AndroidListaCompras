27c1dc5752ae4c4c0264958e134e8e4d3afcb04a
Lucas Cabral Mendes Corrêa <lcmc23@MacBook-Air-de-Lucas.local>
2024-09-11 11:27:56 -0300

    Finalizacao implementacao DB

---
+++ app/src/main/java/lcmc23/github/com/lista_de_compras/ItemDatabase.kt
@@ -0,0 +1,10 @@
+package lcmc23.github.com.lista_de_compras
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [ItemModel::class], version = 1)
+abstract class ItemDatabase : RoomDatabase() {
+
+    abstract fun itemDao(): ItemDao
+}
